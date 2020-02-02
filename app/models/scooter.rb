7f92bccedc1734fef1bd540fda27450534dd23fe
David Roth <david.roth@checkr.com>
2020-02-02 14:05:35 -0800

    Dr/aasm logging (#3)
    * Add logging
    
    * Add query for finding scooters within

--- app/models/scooter.rb
+++ app/models/scooter.rb
@@ -2,30 +2,48 @@ class Scooter < ApplicationRecord
   include AASM
   has_many :transitions
   has_many :tickets
+  scope :active, -> { where("battery > 30").where(status: :locked) }
 
   validates :battery, inclusion: { in: (0..100) }
+  FROM_STATE_EVENT_MAP = {
+    locked: :lock!,
+    unlocked: :unlock!,
+    maintenance: :maintain
+  }.freeze
 
-  aasm whiny_transitions: false, column: :state do
+  aasm column: :state do
     state :locked, initial: true
     state :unlocked, :maintenance, :updating
 
     after_all_transitions :log_transition
 
     event :unlock do
-      transitions from: [:locked, :maintenance], to: :unlocked
+      transitions from: [:updating, :locked], to: :unlocked
     end
 
     event :maintain do
-      transitions from: [:locked, :unlocked], to: :maintenance
+      transitions from: [:updating, :unlocked], to: :maintenance
     end
 
     event :lock do
-      transitions from: [:unlocked, :maintenance], to: :locked
+      transitions from: [:updating, :unlocked], to: :locked
     end
 
     event :update_data do
+      after do |**kwargs|
+        # update battery and location data
+        update_attributes(**kwargs)
+        # return to previous state
+        send(FROM_STATE_EVENT_MAP[aasm.from_state])
+      end
+
       transitions from: [:unlocked, :maintenance, :locked], to: :updating
     end
+  end
+
+  def self.find_active_scooters_within_kilometers(lon, lat, radius)
+    active
+      .where("ST_DWithin(lonlat, ST_MakePoint(#{lon}, #{lat})::geography, #{radius * 1000})")
   end
 
   def log_transition
